2baaba02396e3d7ea83611af2229f82cb5237be9
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-11-01 20:03:22 +0100

    Add insert statement

--- src/main/java/connection/Connection.java
+++ src/main/java/connection/Connection.java
@@ -6,6 +6,11 @@ class Connection {
 
     public static void main(String[] args) {
 
+        insertStatement();
+        selectStatement();
+    }
+
+    private static void selectStatement() {
         try {
             final java.sql.Connection connection = ConnectionProvider.getConnection();
             final Statement statement = connection.createStatement();
@@ -17,6 +22,26 @@ class Connection {
                 final String name = resultSet.getString("name");
                 final int age = resultSet.getInt("age");
                 System.out.printf("Student: %d, %s, %d", id, name, age);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void insertStatement() {
+
+        try (java.sql.Connection connection = ConnectionProvider.getConnection()) {
+            connection.setAutoCommit(false);
+            try {
+                final PreparedStatement statement = connection.prepareStatement("INSERT INTO student (id, name, age) VALUES (?, ?, ?) ");
+                statement.setInt(1, 2);
+                statement.setString(2, "Jan");
+                statement.setInt(3, 20);
+                statement.executeUpdate();
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
             }
         } catch (SQLException e) {
             e.printStackTrace();
